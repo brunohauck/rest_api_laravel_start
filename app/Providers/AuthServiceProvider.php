f309813a79a59f58018d11b6e4b2b4b63fec3026
Bruno Hauck <brunohauck@gmail.com>
2019-07-13 08:13:32 -0300

    Segundo Commit

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Route;
+use Laravel\Passport\Passport;
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+
+        Route::group([ 'middleware' => 'cors'], function() {
+           // \Laravel\Passport\Passport::routes();
+           Passport::routes();
+        });
+
+ 
+    
+    }
+}
